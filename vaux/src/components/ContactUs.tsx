354087a34dc4afc0c1c242d0e4073b1b66a2f969
terror026 <sahilprivate@MacBook-Pro.local>
2023-10-24 13:41:17 +0530

    responsive contact us

--- vaux/src/components/ContactUs.tsx
+++ vaux/src/components/ContactUs.tsx
@@ -4,36 +4,36 @@ import { List, ListItem } from "@mui/material";
 import contactUsbanner from "../assets/contactUs.svg";
 const ContactUs = () => {
 	return (
-		<div className="grid grid-cols-2 gap-4 items-center">
+		<div className="grid  grid-cols-1 md:grid-cols-2 gap-4 items-center sm:text-lg 2xl:text-2xl">
 			<div>
 				<img src={contactUsbanner} alt="contact us" />
 			</div>
-			<Box component="div" id="transition-modal-description">
+			<div id="transition-modal-description">
 				<List sx={{ listStyleType: "disc" }}>
 					<ListItem sx={{ display: "list-item" }}>
 						For any product usage related queries,
-						<Typography component="span" sx={{ fontWeight: "bold" }}>
+						<span className="text-bold">
 							{" "}
 							write to{" "}
 							<a className="text-blue-700" href="mailto:xyz@mail.com">
 								xyz@mail.com
 							</a>
-						</Typography>
+						</span>
 					</ListItem>
 					<ListItem sx={{ display: "list-item" }}>
-						<Typography component="p">
+						<p>
 							If you are a business enterprise looking to buy software for your
 							organization, we'd be glad to assist you!
-							<Typography component="div" sx={{ fontWeight: "bold" }}>
+							<div style={{ fontWeight: "bold" }}>
 								Email us at{" "}
 								<a className="text-blue-700" href="mailto:xyz@mail.com">
 									xyz@mail.com
 								</a>
-							</Typography>
-						</Typography>
+							</div>
+						</p>
 					</ListItem>
 				</List>
-			</Box>
+			</div>
 		</div>
 	);
 };
