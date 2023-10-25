088b51ab773f5dc879c534335f7d2bf531283009
mounica-revuru <mounica@near.com>
2023-10-25 21:25:11 +0530

    merged from pull

--- vaux/src/actions/APIActions.ts
+++ vaux/src/actions/APIActions.ts
@@ -1,1 +1,29 @@
-export {}
+import { VAUX_VOICE_LIST_TYPE, VAUX_VOICE_PREVIEW_TYPE } from "../utils/APITypes";
+import { vauxAPI } from "../utils/NetworkInstance";
+import { VAUX_AI_VOICES_PREVIEW_RESPONSE, VAUX_AI_VOICES_RESPONSE } from "../utils/APIResponseTypes";
+
+export const getAllAIVoiceSample = async () => {
+	const token = "";
+	try {
+		const response = await vauxAPI(token).get<VAUX_AI_VOICES_RESPONSE>(VAUX_VOICE_LIST_TYPE);
+		const { data } = response;
+		if (response.status == 200 && data) {
+			return data;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const fetchAIVoicePreview = async (id:string , name:string) =>{
+	const token = "";
+	try {
+		const response = await vauxAPI(token).get<VAUX_AI_VOICES_PREVIEW_RESPONSE>(VAUX_VOICE_PREVIEW_TYPE + `${id}/${name}`);
+		const {data} = response;
+		if(response.status==200 && data){
+			return data?.S3_link;
+		}
+	} catch (error) {
+		console.log(error);
+		return "";
+	}
+}
--- vaux/src/utils/common.utils.ts
+++ vaux/src/utils/common.utils.ts
@@ -1,1 +1,31 @@
-export {}
+import { VAUX_AI_VOICES_RESPONSE } from "./APIResponseTypes";
+
+export const formatAIVoicesResponseForLanding = (
+	voices: VAUX_AI_VOICES_RESPONSE,
+	MaleCount: number = 2,
+	FemaleCount: number = 2,
+) => {
+	if (!voices?.length) {
+		return [];
+	}
+	let result = [];
+	let limit = {
+		M: 0,
+		F: 0,
+		total: 0,
+	};
+	for (let voice of voices) {
+		if (limit.total === MaleCount + FemaleCount) {
+			break;
+		}
+		if (
+			(voice?.Gender == "M" && limit[voice.Gender] < MaleCount) ||
+			(voice?.Gender == "F" && limit[voice.Gender] < FemaleCount)
+		) {
+			limit[voice.Gender] = limit[voice.Gender] + 1;
+			result.push({ ...voice, img_id: voice.Gender + limit[voice.Gender] });
+			limit.total = limit.total + 1;
+		}
+	}
+	return result;
+};
