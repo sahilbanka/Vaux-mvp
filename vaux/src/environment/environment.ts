319473a1a7ed3df6f8d1cfae6961468ec165901d
terror026 <sahilprivate@MacBook-Pro.local>
2023-10-29 00:37:38 +0530

    base url updated

--- vaux/src/environment/environment.ts
+++ vaux/src/environment/environment.ts
@@ -3,7 +3,7 @@ import { EnvironmentSchema } from './environment-schema';
 
 export const environment: EnvironmentSchema = {
   production: false,
-  baseURL: 'https://57207372-c561-4231-8534-f67979043624.mock.pstmn.io/',
+  baseURL: 'https://3fda-2405-201-9005-3f54-f87e-1443-8d4c-9e13.ngrok-free.app/',
   envName: 'DEVELOPMENT',
   googleClientId: '227470375271-dd2l60nhqr3si84n8u681cmc1qo4fnhc.apps.googleusercontent.com'
 };
