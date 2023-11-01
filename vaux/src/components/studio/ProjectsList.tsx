bd63402a4b3fd82065268ec30bae3df9f09f8219
mounica-revuru <mounica@near.com>
2023-11-01 17:17:28 +0530

    create icon added to auth nav button

--- vaux/src/components/studio/ProjectsList.tsx
+++ vaux/src/components/studio/ProjectsList.tsx
@@ -1,10 +1,11 @@
+import { useAuth } from "hooks/useAuth";
 import { useState } from "react"
 
 function ProjectsList() {
     const [projectsList, setProjectsList] = useState([]);
 
     return (
-        <div className='p-8 ml-[90px] h-full bg-[#f6f7f9]'>
+        <div className='p-8 ml-[90px] bg-[#f6f7f9]'>
             <div className="text-2xl text-black font-semibold">{'Projects'}</div>
             <div>
                 {
