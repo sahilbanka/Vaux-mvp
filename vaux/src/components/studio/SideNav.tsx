d9733b839e5abcb59be9c62e050990da5fdbfaee
terror026 <sahilprivate@192.168.1.8>
2023-11-30 19:56:35 +0530

    reverted keepmount

--- vaux/src/components/studio/SideNav.tsx
+++ vaux/src/components/studio/SideNav.tsx
@@ -39,7 +39,7 @@ function SideNav() {
         openState={openExploreAIsModal}
         onCloseHandler={handleCloseExploreAIsModal}
         MinWidth={"min-w-[70%]"}
-        iskeepMounted={false}
+        iskeepMounted={true}
       >
         <ExploreAI
           handleCloseModal={handleCloseExploreAIsModal}
